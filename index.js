20340bcb7b90c0e64909f6899c8b6d922619330f
tk <git@kiosk.blue>
2023-04-30 22:37:50 -0400

    Comment out description text for now.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 const { BskyAgent, RichText } = bsky
 dotenv.config()
 
-async function start() {
+async function retrieveAndPostMTAStatus() {
   // Read current progress from json file.
   var progress = {
     currentAlertId: 0,
@@ -26,30 +26,30 @@ async function start() {
   const alertsFeed = mtaFeed.filter(entity => /^lmm:alert:\d+$/.test(entity.id))
   const plannedWorkFeed = mtaFeed.filter(entity => /^lmm:planned_work:\d+$/.test(entity.id))
 
+  var alertMessage = ""
   if (alertsFeed.length > 0 && alertsFeed[0].id.localeCompare(progress.currentAlertId) > 0) {
     const alert = alertsFeed[0].alert
-    var alertMessage = ""
     if (alert.header_text) {
       alertMessage += alert.header_text.translation[0].text
     }
-    if (alert.description_text) {
+    /*if (alert.description_text) {
       alertMessage += '\n' + alert.description_text.translation[0].text
-    }
+    }*/
 
     console.log("Alert message: ", alertMessage)
     console.log("Alert: ", alert)
     progress.currentAlertId = alertsFeed[0].id
   }
 
+  var plannedWorkMessage = ""
   if (plannedWorkFeed.length > 0 && plannedWorkFeed[0].id.localeCompare(progress.currentPlannedWorkId) > 0) {
     const alert = plannedWorkFeed[0].alert
-    var plannedWorkMessage = ""
     if (alert.header_text) {
       plannedWorkMessage += alert.header_text.translation[0].text
     }
-    if (alert.description_text) {
+    /*if (alert.description_text) {
       plannedWorkMessage += '\n' + alert.description_text.translation[0].text
-    }
+    }*/
 
     console.log("Planned work message: ", plannedWorkMessage)
     console.log("Planned work alert: ", alert)
@@ -59,13 +59,24 @@ async function start() {
   // Log into bluesky and post
   const agent = new BskyAgent({ service: 'https://bsky.social/' })
   await agent.login({ identifier: process.env.BLUESKY_ACCOUNT, password: process.env.BLUESKY_PASSWORD})
-  //console.log("Agent DID: ", agent.session.did)
 
-  /*const feed = await agent.getAuthorFeed({
-      actor: agent.session.did
-  })
-  console.log("Feed: ", feed)*/
+  if (alertMessage) {
+    const alertRichText = new RichText({ text: alertMessage })
+    await alertRichText.detectFacets(agent)
+    agent.post({
+      text: alertRichText.text,
+      facets: alertRichText.facets
+    })
+  }
 
+  if (plannedWorkMessage) {
+    const plannedWorkRichText = new RichText({ text: plannedWorkMessage })
+    await plannedWorkRichText.detectFacets(agent)
+    agent.post({
+      text: plannedWorkRichText.text,
+      facets: plannedWorkRichText.facets
+    })
+  }
 
   // Write update to progress file
   fs.writeFileSync('progress.json', JSON.stringify(progress))
