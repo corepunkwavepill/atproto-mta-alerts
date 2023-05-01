388200a0d572712f770fd1970a6b8df8bd3d750e
tk <git@kiosk.blue>
2023-04-30 23:06:24 -0400

    Fix function name.

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ async function retrieveAndPostMTAStatus() {
 var three_minutes = 3 * 60 * 1000
 setInterval(async () => {
   console.log("Running MTA Subway Script");
-  await start()
+  await retrieveAndPostMTAStatus()
 }, three_minutes)
